7caff7f8cbd4ea5436bd0de0ca89a5e3ea63d0fc
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2019-10-11 23:57:47 +0700

    test

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>tugas1 (Backup 1570811206389)</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.min.css">
+</head>
+
+<body>
+    <div class="login-clean">
+        <form method="post">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-cloud"></i></div>
+            <h2 align="center">Tugas 1 Kelompok 2</h2><div style="text-align:center;">
+    <a class='btn btn-primary' href='index2.php'>No. 15 / 101162xx <br> Andreas Rizki Rizaldi</a>
+    <a class='btn btn-primary' href='https://tugas1-101162xx-20.herokuapp.com/'>No. 20 / 101162xx <br> Adam Aris Rizaldy</a>
+    <a class='btn btn-primary' href='https://tugas1-101162xx-44.herokuapp.com'>No. 44 / 101162xx <br>Andika Dirgantara</a>
+    <a class='btn btn-primary' href='https://tugas1-101163xx-31.herokuapp.com/'>No. 31 / 101163xx <br> M. Adityo Fathur R</a>
+    <a class='btn btn-primary' href='https://tugas1-101163xx-33.herokuapp.com/'>No. 33 / 101163xx <br> Rizqi Adiguna</a><br>
+    <a class='btn btn-secondary' href='index2.php' style='margin:20px'>Download Review Security</a>
+</div></form>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ index2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>tugas1 (Backup 1570811206389)</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.min.css?h=5cde54d618d121bac13974cd79401926">
+</head>
+
+<body>
+    <div class="login-clean">
+        <form method="post">
+            <h2 class="sr-only">Login Form</h2>
+            <h2 align="center">NIM : 10116281</h2>
+            <h2 align="center">Nama : Andreas Rizki Rizaldi</h2>
+        </form>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
